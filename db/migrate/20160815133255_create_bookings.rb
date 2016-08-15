77271dd94cded3989dd3dba2738ffac82e9a9ca7
sadhia mengal <sadhia.mengal@gmail.com>
2016-08-15 16:24:44 +0200

    booking model, validations, associations and reference

--- db/migrate/20160815133255_create_bookings.rb
+++ db/migrate/20160815133255_create_bookings.rb
@@ -1,7 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[5.0]
   def change
     create_table :bookings do |t|
-
+      t.references :barber, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
