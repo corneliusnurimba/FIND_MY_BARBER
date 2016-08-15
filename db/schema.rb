e4c3612df855cfcd76512ef83dbdfbaffd2ffacb
Sara Wulff Kringelbach <sara_kringelbach@hotmail.com>
2016-08-15 18:13:08 +0200

    user devise and a bit of scss

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,9 @@ ActiveRecord::Schema.define(version: 20160815121648) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
